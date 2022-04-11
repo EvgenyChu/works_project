75358941b383aca81ea4f8db48917b08c3862795
EvgenyChu <79277702141@ya.ru>
2022-04-11 19:15:12 +0300

    start works project

---
+++ app/src/main/java/com/template/Utils.kt
@@ -0,0 +1,16 @@
+package com.template
+
+import android.content.Context
+import android.net.ConnectivityManager
+import android.net.NetworkInfo
+import androidx.core.content.ContextCompat.getSystemService
+
+object Utils {
+    private fun isNetworkAvailable(): Boolean {
+        val connectivityManager = App.applicationContext().getSystemService(Context.CONNECTIVITY_SERVICE)
+        return if (connectivityManager is ConnectivityManager) {
+            val networkInfo: NetworkInfo? = connectivityManager.activeNetworkInfo
+            networkInfo?.isConnected ?: false
+        } else false
+    }
+}
